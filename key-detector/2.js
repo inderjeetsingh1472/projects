e2e0ffc2b5fbcc16a788fd226675bca7b2edc1ca
inderjeet1472 <gate37814@gmail.com>
2024-05-10 17:11:05 +0530

    p

--- key-detector/2.js
+++ key-detector/2.js
@@ -2,3 +2,12 @@ const gameName = new String('hitesh-hc-com')
 
 console.log(gameName[0]);
 let inder=console.log(gameName.__proto__);
+var i=35;
+const inderj=()=>{
+    var i=5;
+  return  console.log(i);
+    
+}
+inderj()
+console.log(i);
+
